752f69470f5ef4e4af60a6b1273bcd3f900b2907
LeonamLoureiro <101669604+LeonamLoureiro@users.noreply.github.com>
2023-06-13 21:31:16 -0300

    button menu

--- menu.js
+++ menu.js
@@ -1,2 +1,18 @@
 const navbar = $("nav");
-const menuOpen = $
+const menuOpen = document.querySelector("#menu-open");
+const menuClose = document.querySelector("#menu-close");
+console.log(navbar);
+menuOpen.addEventListener("click", () => {
+    console.log("I'm being clicked");
+    
+    menuClose.style.display = "block";
+    menuOpen.style.display = "none";
+  });
+
+  menuClose.addEventListener("click", () => {
+    console.log("Close clicked");
+    
+    menuClose.style.display = "none";
+    menuOpen.style.display = "block";
+    
+  });
