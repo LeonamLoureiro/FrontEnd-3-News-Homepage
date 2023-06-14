86c0d583bff38ea836f2a6a4231be488dcf36d06
LeonamLoureiro <101669604+LeonamLoureiro@users.noreply.github.com>
2023-06-14 16:30:02 -0300

    toggle menu

--- menu.js
+++ menu.js
@@ -1,18 +1,30 @@
-const navbar = $("nav");
+const navbar = document.querySelector(".navbar-collapse");
 const menuOpen = document.querySelector("#menu-open");
 const menuClose = document.querySelector("#menu-close");
-console.log(navbar);
+
 menuOpen.addEventListener("click", () => {
-    console.log("I'm being clicked");
-    
+     
     menuClose.style.display = "block";
     menuOpen.style.display = "none";
+    navbar.style.display = "block";
+    navbar.classList.add("navbar-collapse-fixed");
+    navbar.classList.remove("navbar-list");
   });
 
   menuClose.addEventListener("click", () => {
-    console.log("Close clicked");
-    
+        
     menuClose.style.display = "none";
     menuOpen.style.display = "block";
+    navbar.style.display = "none";    
     
+  });
+
+  window.addEventListener('scroll', function() {
+    var fixedNavbar = document.querySelector('.navbar-collapse-fixed');
+    var positionViewport = window.innerHeight;
+  
+    if (window.scrollY >= positionViewport) {
+      fixedNavbar.classList.remove('navbar-collapse-fixed');
+      navbar.style.display = "none";
+    }
   });
